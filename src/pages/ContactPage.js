1a873f0711b556190d235356fde8314f8f5e02ae
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2024-10-27 18:09:11 +0100

    added ReCaptcha Disable

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -9,23 +9,23 @@ function ContactPage() {
     <div className="flex flex-col min-h-screen justify-between relative overflow-hidden">
       <Header />
 
-      {/* Main Section with Video Background */}
       <main className="flex flex-col items-center justify-center flex-grow text-center relative z-20 p-6">
         <div className="relative z-20 p-5 text-white bg-black bg-opacity-50 rounded-xl mx-2 sm:mx-4">
           <h1 className="text-3xl sm:text-4xl font-bold mb-4">Get in Touch</h1>
 
-          {/* Introduction */}
           <p className="text-base sm:text-lg text-gray-400 mb-6">
             I’m excited to connect! Feel free to reach out if you’d like to
             collaborate or have any questions.
           </p>
 
-          {/* Contact Form */}
           <form
             action="https://formsubmit.co/pierre.wy@outlook.fr?redirect=success.html"
             method="POST"
             className="flex flex-col space-y-4 w-full max-w-md mx-auto border border-indigo-400"
           >
+            {/* Disable reCAPTCHA */}
+            <input type="hidden" name="_captcha" value="false" />
+
             <input
               type="text"
               name="name"
