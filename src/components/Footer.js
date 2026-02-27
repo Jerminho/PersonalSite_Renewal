11012f0895e2cbf85d7a0c0e0c9241dcf0b80172
Pierre Njiné Happy <pierre.wy@outlook.fr>
2026-02-27 02:22:28 +0100

    minor change in Homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="p-4 text-center shadow-inner z-20">
-      <p className="text-gray-300">© 2024 built by NH - All Rights Reserved</p>
+      <p className="text-gray-300">© 2026 built by NH - All Rights Reserved</p>
     </footer>
   );
 };
